bf1b8dd768fbcae0ec136617baaff2233b8e018d
ivagmir <ivagmir@gmail.com>
2026-05-22 07:33:25 -0600

    Add sitemap function to generate site URLs

--- pacific-prime-engineering/app/sitemap.ts
+++ pacific-prime-engineering/app/sitemap.ts
@@ -1,1 +1,14 @@
-
+export default function sitemap() {
+  return [
+    {
+      url: "https://pacificprimeengineering.com",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://pacificprimeengineering.com/thank-you",
+      lastModified: new Date(),
+      priority: 0.5,
+    },
+  ];
+}
