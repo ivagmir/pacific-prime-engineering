5067c1858f0b9e33ee8f3eebbb81a390ba9a3ddf
ivagmir <ivagmir@gmail.com>
2026-05-25 12:38:37 -0600

    Add Google Analytics tracking functionality

--- pacific-prime-engineering/app/lib/gtag.ts
+++ pacific-prime-engineering/app/lib/gtag.ts
@@ -1,1 +1,17 @@
+export const GA_MEASUREMENT_ID =
+  "G-X3GPDNRXJV";
 
+declare global {
+  interface Window {
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
+export const trackEvent = (
+  eventName: string,
+  params?: Record<string, unknown>
+) => {
+  if (typeof window !== "undefined") {
+    window.gtag?.("event", eventName, params);
+  }
+};
