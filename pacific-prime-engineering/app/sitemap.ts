4f79edfc30b6636037c6a17e0674aaeaaba140c7
ivagmir <ivagmir@gmail.com>
2026-06-16 16:27:23 -0600

    sitemap

--- pacific-prime-engineering/app/sitemap.ts
+++ pacific-prime-engineering/app/sitemap.ts
@@ -10,5 +10,70 @@ export default function sitemap() {
       lastModified: new Date(),
       priority: 0.5,
     },
+    {
+      url: "https://www.pacificprimeengineering.com/services/hospitality-engineering",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/emergency-response",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/water-pumping-systems",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/wastewater-grinder-sewage",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/civil-infrastructure-engineering",
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/pools-jacuzzi-systems",
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/building-maintenance-repairs",
+      lastModified: new Date(),
+      priority: 0.6,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/telecom-cctv-networks",
+      lastModified: new Date(),
+      priority: 0.6,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/hvac-chillers-heaters-lpgas",
+      lastModified: new Date(),
+      priority: 0.5,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/energy-solutions",
+      lastModified: new Date(),
+      priority: 0.5,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/solar-networks",
+      lastModified: new Date(),
+      priority: 0.5,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/automation-controls",
+      lastModified: new Date(),
+      priority: 0.5,
+    },
+    {
+      url: "https://www.pacificprimeengineering.com/services/industrial-maintenance",
+      lastModified: new Date(),
+      priority: 0.5,
+    },
   ];
 }
